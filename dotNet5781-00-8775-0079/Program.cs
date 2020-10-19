4f4d9bedd24410a503ecd694cea6129812cc4a0d
Gila Bracha <gilabracha@gmail.com>
2020-10-19 15:27:57 +0300

    Initial code & Welcome 0079

---
+++ dotNet5781-00-8775-0079/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace dotNet5781_00_8775_0079
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8775();
+            Welcome0079();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0079();
+        private static void Welcome8775()
+        {
+            Console.WriteLine("Enter your name:"); //cout
+            string userName = Console.ReadLine(); //cin char* name; cin<<name; 
+            Console.WriteLine("{0}, welcome to my first console application", userName); //cout>> name >> "welcome to my first console.."
+        }
+    }
+}
