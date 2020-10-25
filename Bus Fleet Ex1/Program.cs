ff393bcd7e351335ccdf1c383473bcc965ff6331
Gila Bracha <gilabracha@gmail.com>
2020-10-25 16:00:34 +0200

    Set up bus fleet trial

---
+++ Bus Fleet Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bus_Fleet_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bus fleet info 
+        }
+    }
+}
