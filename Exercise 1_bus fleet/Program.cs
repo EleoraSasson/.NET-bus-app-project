8a7cbc7562d726357e5362c1746df4680b7f9e08
Eléora Sasson <eleora.sasson@gmail.com>
2020-10-25 14:37:12 +0100

    plan exercise 1

---
+++ Exercise 1_bus fleet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1_bus_fleet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+/*define list of buses (class)
+ * switch statement for the main (menu of options for the user)
+ * print the menu so the user can select the function
+ */
+
+/*license number
+ * mileage
+ * fuel information
+ */
+
+/*menu:
+ * A. adding a bus
+ * B. choosing a bus
+ * C. refuel or maintenance 
+ * D. display of mileage since last maintenance
+ * E. exit
+ */
