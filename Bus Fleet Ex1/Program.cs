4c9bd49d5610a204dbd3345c0b86692379aae898
Eléora Sasson <eleora.sasson@gmail.com>
2020-10-25 15:05:00 +0100

    merging our files

--- Bus Fleet Ex1/Program.cs
+++ Bus Fleet Ex1/Program.cs
@@ -14,3 +14,25 @@ namespace Bus_Fleet_Ex1
         }
     }
 }
+
+
+//main
+/*define list of buses (class)
+ * switch statement for the main (menu of options for the user)
+ * print the menu so the user can select the function
+ */
+
+//bus class
+/*license number
+ * mileage
+ * fuel information
+ */
+
+//menu:
+/* A. adding a bus
+* B. choosing a bus
+* C. refuel or maintenance 
+* D. display of mileage since last maintenance
+* E. exit
+*/
+
