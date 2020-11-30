502cf45d7c0f0553c937779ae317399f87ef9376
Gila Bracha <gilabracha@gmail.com>
2020-11-30 16:37:50 +0200

    getting changes

--- Ex3b_GUI/MainWindow.xaml.cs
+++ Ex3b_GUI/MainWindow.xaml.cs
@@ -66,7 +66,7 @@ namespace Ex3b_GUI
         public MainWindow()
         {
             InitializeComponent();
-            DG_Buses.ItemsSource = BusList;
+            
         }
 
         public DateTime startDate;
