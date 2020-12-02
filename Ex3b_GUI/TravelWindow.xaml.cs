fc4a913157359b880bf3cb7f54ea470c790acf6a
Eléora Sasson <eleora.sasson@gmail.com>
2020-12-02 01:07:43 +0100

    trying to sync

---
+++ Ex3b_GUI/TravelWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ex3b_GUI
+{
+    /// <summary>
+    /// Interaction logic for TravelWindow.xaml
+    /// </summary>
+    public partial class TravelWindow : Window
+    {
+        int distance;
+        public TravelWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string dist = TravelMileTextBox.Text;
+            distance = Convert.ToInt32(dist);
+        }
+
+        private void TravelMileTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //this.
+        }
+    }
+}
