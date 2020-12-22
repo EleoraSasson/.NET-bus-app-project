18e6a1adbfd6ab0185c1ebfe105c5fdac2b28293
Eléora Sasson <eleora.sasson@gmail.com>
2020-12-22 17:38:24 +0100

    completed the user and line bus

--- DALApi/LineStation.cs
+++ DALApi/LineStation.cs
@@ -8,5 +8,45 @@ namespace DO
 {
     public class LineStation :ICloneable
     {
+        private string id; //string but can be an int, will see after
+        public string lineID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string station;
+
+        public string stationCode
+        {
+            get { return station; }
+            set { station = value; }
+        }
+
+        private string key;
+
+        public string entityKey
+        {
+            get { return lineID + stationCode; }
+            set { key = lineID + stationCode; }
+        }
+
+
+        private int stationNum;
+
+        public int stationNumber //number of stations on the line
+        {
+            get { return stationNum; }
+            set { stationNum = value; }
+        }
+
+        //add ctor
+        //additional information?
+
+        public override string ToString()
+        {
+            return "Line station details: \n Line ID: " + lineID + "  \n Station code: " + stationCode + " \n Station number: " + stationNum;
+        }
+
     }
 }
--- DALApi/User.cs
+++ DALApi/User.cs
@@ -8,5 +8,31 @@ namespace DO
 {
     public class User :ICloneable
     {
+        private string name;
+
+        public string userName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string password;
+
+        public string userPassword   //should we add properties for a password?
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private int permission;
+
+        public int adminPermission //??
+        {
+            get { return permission; }
+            set { permission = value; }
+        }
+
+
+
     }
 }
