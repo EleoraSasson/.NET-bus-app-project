8ef34e19f7f69df7dafb356569994815d51b421d
Gila Bracha <gilabracha@gmail.com>
2020-12-23 21:29:48 +0200

    added outline of BLApi for BL layer

---
+++ BLApi/BLImp1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALApi;
+using DO;
+using BO;
+
+namespace BLApi
+{
+    class BLImp1
+    {
+    }
+}
---
+++ BLApi/IBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+
+namespace BLApi
+{
+    public interface IBL
+    {
+        //has methods that the BL object will implement
+
+    }
+}
